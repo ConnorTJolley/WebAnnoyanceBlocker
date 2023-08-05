3ee607226362c8adfc943bf11a93e7b3548d71bb
Connor Jolley <connorjolley18@gmail.com>
2023-08-06 00:43:53 +0100

    Adjustments
    Trimmed unused CSS and adjusted `content.js` to have named constants for DOM element names

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,16 @@
+// fc named vars
+const fc_consentRoot = ".fc-consent-root";
+const fc_manageOptionsBtn = "fc-button fc-cta-manage-options fc-secondary-button";
+const fc_generalInterestsCheckBoxes = "fc-preference-legitimate-interest purpose";
+const fc_publisherInterestsCheckBoxes = "fc-preference-legitimate-interest publisher-purpose";
+const fc_confirmBtn = "fc-button fc-confirm-choices fc-primary-button";
+
 function checkForFcConsentRoot() 
 {
     // Set current pages tally to 0
     chrome.storage.local.set({"currentPageTally": 0 });
 
-    var targetNode = document.querySelectorAll(".fc-consent-root")[0];
+    var targetNode = document.querySelectorAll(consentRoot)[0];
     if(!targetNode) 
     {
         window.setTimeout(checkForFcConsentRoot,500);
@@ -11,7 +18,7 @@ function checkForFcConsentRoot()
     }
     else
     {
-        var manageOptionsBtn = document.getElementsByClassName("fc-button fc-cta-manage-options fc-secondary-button")[0];
+        var manageOptionsBtn = document.getElementsByClassName(fc_manageOptionsBtn)[0];
         if (manageOptionsBtn !== null && manageOptionsBtn !== undefined)
         {
             // Click btn
@@ -29,13 +36,13 @@ function checkForFcConsentRoot()
                 var currentTally = 0;
 
                 //// Preference for general purposes
-                [...document.getElementsByClassName("fc-preference-legitimate-interest purpose")].forEach(element => {
+                [...document.getElementsByClassName(fc_generalInterestsCheckBoxes)].forEach(element => {
                     element.checked = false; // Uncheck
                     currentTally++;
                 });
     
                 //// 'Publisher' preferences
-                [...document.getElementsByClassName("fc-preference-legitimate-interest publisher-purpose")].forEach(element => {
+                [...document.getElementsByClassName(fc_publisherInterestsCheckBoxes)].forEach(element => {
                     element.checked = false; // Uncheck
                     currentTally++;
                 });
@@ -44,11 +51,11 @@ function checkForFcConsentRoot()
                 chrome.storage.local.set({"allTimeTally": totalTally});
                 chrome.storage.local.set({"currentPageTally": currentTally });
 
-                var confirmElement = document.getElementsByClassName("fc-button fc-confirm-choices fc-primary-button")[0];
+                var confirmElement = document.getElementsByClassName(fc_confirmBtn)[0];
                 confirmElement.click();
             });
         }
     }
 }
 
-checkForFcConsentRoot();
+checkForFcConsentRoot(); // fc Handler
